9e4bf401cda88440133b4fd6c238d1e812770ba8
Manish Varma <manishvarma2972@gmail.com>
2026-09-19 16:48:51 +0530

    product_of_three_numbers.py

---
+++ product_of_three_numbers.py
@@ -0,0 +1,6 @@
+print ("To multiply three numbers.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the seconf number: "))
+num3 = int(input("Enter the third number: "))
+product = num1 * num2 * num3
+print ("The product of ", num1, ", ", num2, " and ", num3, " is ", product, ".", sep="")
