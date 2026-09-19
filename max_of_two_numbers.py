5dfd3ef547950b1ef4afd4a85da47de9975645c7
Manish Varma <manishvarma2972@gmail.com>
2026-09-19 17:00:18 +0530

    max_of_two_numbers.py

---
+++ max_of_two_numbers.py
@@ -0,0 +1,7 @@
+print ("To compare two numbers.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if (num1 > num2):
+    print (num1, " is bigger than ", num2, ".", sep="", end="")
+if (num2 > num1):
+    print (num2, " is bigger than ", num1, ".", sep="", end="")
